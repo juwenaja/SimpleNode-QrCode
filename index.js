030c2830573a5b3885207463315098a8fd9ea577
Juwennn <76975675+juwenaja@users.noreply.github.com>
2023-07-31 09:31:47 +0700

    gabut

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * THIS SCRIPT ONLY USING CMD/TERMINAL
+ * USE: > npm start "url/text"
+ * 
+ * WARNING: INPUT URL DON'T USE ['https://']
+ * 
+ * Thanks To : https://github.com/SumiMakito
+ * GENERATE QR CODE SCRIPT USING : https://github.com/SumiMakito/Awesome-qr.js
+ */
+
+const lib = require("./lib/lib");
+
+const index = async() => {
+    let txt2 = process.argv[2] || lib.log || false;
+    await lib.startLog(txt2)
+    await lib.process(txt2)
+    await lib.succsessLog(txt2)
+}
+
+index()
+.catch(console.error);
